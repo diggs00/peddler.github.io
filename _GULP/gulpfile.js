2730149d390a486c0e7d817587c8e80330593ce1
David Baker <dbaker@k6b9.com>
2018-03-21 18:00:19 -0500

    node mopds new

--- _GULP/gulpfile.js
+++ _GULP/gulpfile.js
@@ -1,13 +1,9 @@
-// Include gulp
 var gulp = require('gulp');
-
-// Include Plugins
 var express = require('express');
 var browserSync = require('browser-sync');
-var gutil = require('gulp-util');
-var sass = require('gulp-ruby-sass');
-var minifyCSS = require('gulp-minify-css');
-var watch = require('gulp-watch');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var cleanCss = require('gulp-clean-css');
 var notify = require('gulp-notify');
 var server;
 var html;
@@ -20,7 +16,6 @@ gulp.task('server' , function() {
     browserSync({proxy: 'localhost:6000'});
 })
 
-
 function reload() {
     if(server) {
         return browserSync.reload({stream: true});
@@ -29,29 +24,34 @@ function reload() {
 }
 
 
-// Watch TASK
-gulp.task('watch', function() {
-    gulp.watch('../sass/*scss', ['sass'])  // Watch all the .less files, then run the less task
-    gulp.watch('../**/*.html', ['html']);  // Watch all the .html files, then run the RELOAD HTML task
-});
-
-
-// Compile LESS TASK
-gulp.task('sass', function () {
-    return sass('../sass/main.scss') //path to your main less files
-    .pipe(minifyCSS())
-    .pipe(gulp.dest('../css')) // css output folder
+// Compiles sass files to css
+gulp.task('sass', function(){
+    gulp.src('../scss/my-account.scss')
+  	.pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(cleanCss())
+    .pipe(gulp.dest('../dist/css'))
     .pipe(reload())
     .pipe(notify({ message: 'SASS task complete' }));
-
 });
+
 
 // RELOAD HTML TASK
 gulp.task('html' , function() {
-    gulp.src('../**/*.html')
+    gulp.src('../*.html')
     .pipe(reload());
 })
 
 
+// Watches for any sass changes and HTML changes
+gulp.task('watch', function (){
+  	gulp.watch(['../scss/**/*.scss', '../vendor/bootstrap/scss/**/*.scss'], ['sass'])
+  	gulp.watch('../**/*.html', ['html']);
+});
+
 
 gulp.task('default', ['server', 'watch', 'sass', 'html']); // Default will run the 'entry' watch task
+
+
+
